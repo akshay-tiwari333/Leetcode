88c91dca81b0369d8939e60e580a5e712c14025a
akshay-tiwari333 <tiwariakshay799@gmail.com>
2026-08-09 13:00:29 +0530

    Time: 1 ms (98.15%), Space: 47.2 MB (20.21%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,31 +1,22 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        int[][] res=new int[intervals.length+1][2];
-        int k=0;
-        for(int i=0;i<intervals.length;i++){
-            res[k++]=intervals[i];
-        }
-        res[k]=newInterval;
-      
+        int i=0;
         List<int[]> ans=new ArrayList<>();
-        Arrays.sort(res,Comparator.comparing(i->i[0]));
-        int s=res[0][0];
-        int e=res[0][1];
-        for(int i=1;i<res.length;i++){
-            int ss=res[i][0];
-            int ee=res[i][1];
-            if(e>=ss){
-                e=Math.max(e,ee);
-            }
-            else{
-                ans.add(new int[]{s,e});
-                s=ss;
-                e=ee;
-            }
+        while(i<intervals.length && intervals[i][1]<newInterval[0]){
+            ans.add(intervals[i]);
+            i++;
         }
-        ans.add(new int[]{s,e});
+        while(i<intervals.length && intervals[i][0]<=newInterval[1]){
+            newInterval[0]=Math.min(intervals[i][0],newInterval[0]);
+            newInterval[1]=Math.max(intervals[i][1],newInterval[1]);
+            i++;
+        }
+        ans.add(newInterval);
+        while(i<intervals.length){
+            ans.add(intervals[i]);
+            i++;
+        }
         return ans.toArray(new int[ans.size()][]);
-
         
     }
 }

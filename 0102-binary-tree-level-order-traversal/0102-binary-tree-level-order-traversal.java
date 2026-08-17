96d6b3f3fb603bdf9f41af66ff5835ad7c3a394d
akshay-tiwari333 <tiwariakshay799@gmail.com>
2026-08-17 22:58:37 +0530

    Time: 14 ms (1.62%), Space: 46.9 MB (42.03%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -15,26 +15,30 @@
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        Queue<TreeNode> q=new LinkedList<>();
         List<List<Integer>> res=new ArrayList<>();
-        if(root!=null) q.add(root);
-        while(q.size()>0){
+        int level=height(root)+1;
+        if(root==null) return res;
+        for(int i=1;i<=level;i++){
             List<Integer> ans=new ArrayList<>();
-            int s=q.size();
-            for(int i=0;i<s;i++){
-            TreeNode top=q.peek();
-            if(top.left!=null)q.add(top.left);
-            if(top.right!=null)q.add(top.right);
-            ans.add(q.peek().val);
-            q.remove();
-
-
-            } 
+            nth(root,i,ans);
             res.add(ans);
-
         }
         return res;
-
         
+    }
+    public void nth(TreeNode root,int n,List<Integer> ans){
+        if(root==null) return;
+        if(n==1){
+            ans.add(root.val);
+            return;
+        }
+        nth(root.left,n-1,ans);
+        nth(root.right,n-1,ans);
+    }
+    public int height(TreeNode root){
+        if(root==null) return 0;
+        if(root.left==null && root.right==null) return 0;
+        return 1+Math.max(height(root.left),height(root.right));
+
     }
 }

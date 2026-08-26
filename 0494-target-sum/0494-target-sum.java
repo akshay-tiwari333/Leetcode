b8f2f4a09517252b704781158d703f32c0d3f097
akshay-tiwari333 <tiwariakshay799@gmail.com>
2026-08-26 13:55:19 +0530

    Time: 10 ms (52.74%), Space: 46.8 MB (26.55%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,9 +1,23 @@
 class Solution {
+    int[][] dp;
+    int total;
     public int findTargetSumWays(int[] nums, int target) {
+       
+        total = 0;
+
+        for(int i = 0; i < nums.length; i++) {
+            total += nums[i];
+        }
+
+        dp = new  int[nums.length][2 * total + 1];
+        for(int i=0;i<nums.length;i++){
+            Arrays.fill(dp[i],-1);
+        }
         return helper(nums,target,0);
         
     }
     public int helper(int[] nums,int t,int i){
+        if(Math.abs(t) > total) return 0;
         
         if(i==nums.length){
             if(t==0){
@@ -13,8 +27,9 @@ class Solution {
                 return 0;
             }
         }
+        if(dp[i][t+total]!=-1) return dp[i][t+total];
         int p=helper(nums,t+nums[i],i+1);
         int n=helper(nums,t-nums[i],i+1);
-        return p+n;
+        return dp[i][t+total]=p+n;
     }
 }

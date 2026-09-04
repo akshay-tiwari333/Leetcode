539fab9bfe6df89b4646baf6235430277902d3c7
akshay-tiwari333 <tiwariakshay799@gmail.com>
2026-09-04 12:27:09 +0530

    Time: 73 ms (22.61%), Space: 274.6 MB (5.85%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,5 +1,7 @@
 class Solution {
+    Boolean[][] dp;
     public int countSubstrings(String s) {
+        dp=new Boolean[1000][1000];
         int c=0;
         for(int i=0;i<s.length();i++){
             for(int j=i;j<s.length();j++){
@@ -13,9 +15,10 @@ class Solution {
     }
     public boolean helper(String s,int i,int j){
         if(i>j) return true;
+        if(dp[i][j]!=null) return dp[i][j];
         if(s.charAt(i)==s.charAt(j)){
-            return  helper(s,i+1,j-1);
+            return dp[i][j]= helper(s,i+1,j-1);
         }
-        return false;
+        return dp[i][j]= false;
     }
 }

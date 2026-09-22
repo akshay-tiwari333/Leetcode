acf5e4449466efa32c7c05a8d1cd47a009d62703
akshay-tiwari333 <tiwariakshay799@gmail.com>
2026-09-22 10:28:39 +0530

    Time: 34 ms (18.51%), Space: 47 MB (14.17%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,15 +1,27 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
         int n=s1.length();
-        char[] a=s1.toCharArray();
-        Arrays.sort(a);
-        String ans=new String(a);
-        for(int i=0;i<=s2.length()-s1.length();i++){
-            String st=s2.substring(i,i+n);
-            char[] b=st.toCharArray();
-            Arrays.sort(b);
-            String res=new String(b);
-            if(ans.equals(res)) return true; 
+        int m=s2.length();
+        Map<Character,Integer> hm1=new HashMap<>();
+        Map<Character,Integer> hm2=new HashMap<>();
+        for(int i=0;i<n;i++){
+            hm1.put(s1.charAt(i),hm1.getOrDefault(s1.charAt(i),0)+1);
+        }
+        int j=0;
+        for(int i=0;i<m;i++){
+             hm2.put(s2.charAt(i),hm2.getOrDefault(s2.charAt(i),0)+1);
+            if((i-j+1)==n){
+                 if(hm1.equals(hm2)) return true;
+                hm2.put(s2.charAt(j),hm2.get(s2.charAt(j))-1);
+                if(hm2.get(s2.charAt(j))==0){
+                    hm2.remove(s2.charAt(j));
+                }
+                j++;
+            }
+            
+          
+           
+
         }
         return false;
         
